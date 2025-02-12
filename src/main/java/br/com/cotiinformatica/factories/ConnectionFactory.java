173f4869ddd1bc55a6b0bbd57b008000c7d2d65b
cotiaws <cotiaws5@gmail.com>
2025-02-11 21:43:00 -0300

    Criação da fábrica de conexão e classes do repositório

---
+++ src/main/java/br/com/cotiinformatica/factories/ConnectionFactory.java
@@ -0,0 +1,25 @@
+package br.com.cotiinformatica.factories;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+
+	/*
+	 * Método para abrir conexão com o banco de dados
+	 */
+	public Connection getConnection() {
+		try {
+						
+			var host = "jdbc:postgresql://localhost:5436/apiprodutos";
+			var user = "admin";
+			var pass = "admin123";
+			
+			return DriverManager.getConnection(host, user, pass);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
